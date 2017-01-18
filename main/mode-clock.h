5485a706e675249a605aafe60e279c97c203e224
Dermot Duffy <dermot.duffy@gmail.com>
2017-01-18 16:33:58 +0000

    Remove superfluous function declarations.

--- main/mode-clock.h
+++ main/mode-clock.h
@@ -13,8 +13,4 @@ extern SemaphoreHandle_t mode_clock_semaphore;
 
 void mode_clock_init(displaybuffer_t* displaybuffer);
 
-static void task_mode_clock();
-static void draw_hours(int hours, displaybuffer_t* displaybuffer);
-static void draw_minutes(int mins, displaybuffer_t* displaybuffer);
-
 #endif
